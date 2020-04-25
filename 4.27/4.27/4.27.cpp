acc483d1860d43097b9916ef38dac2ea627fee88
marc4123 <marcoelda0@gmail.com>
2020-04-24 19:23:14 -0500

    Problema 4.27

---
+++ 4.27/4.27/4.27.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a, b, c, d, e;
+    cin >> a >> b >> c >> d >> e;
+    if (a == e)
+    {
+        if (b == d)
+        {
+            cout << "es un palindromo";
+        }
+        else
+        {
+            cout << "no es un palindromo";
+        }
+    }
+    else
+    {
+        cout << "no es un palindromo";
+    }
+    return 0;
+}
